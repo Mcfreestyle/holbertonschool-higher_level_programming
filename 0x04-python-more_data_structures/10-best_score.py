4b0aa84402eddfd02b527238a294fb38f5dded95
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-05 19:01:09 -0500

    chore: use of default parameter of max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return (None)
 
-    max_score = max(a_dictionary.values())
+    max_score = max(a_dictionary.values(), default=None)
     for k, v in a_dictionary.items():
         if v == max_score:
             return (k)
