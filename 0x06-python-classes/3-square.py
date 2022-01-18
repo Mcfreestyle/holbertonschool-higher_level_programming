b635bf7b59ea28896b2c64fe8f15388c4468cf47
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-18 18:30:04 -0500

    feat: class that defines the area method that returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module defines a square class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """
+        Defines the size attribute of the square
+
+        Args:
+            size (:obj:`int`, optional): size of the square
+
+        Raises:
+            TypeError: If size is not integer
+            VaueError: If size is less than 0
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+
+        Returns:
+            the area of the square
+        """
+        area = self.__size ** 2
+        return (area)
