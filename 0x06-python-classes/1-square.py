ff2be42ac5a803b9c13ef5230169553e182dad09
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-18 17:03:49 -0500

    feat: square class that defines the size attribute of the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a square class"""
+
+
+class Square:
+    """class that has a method"""
+    def __init__(self, size):
+        """
+        Initializes the size of the square
+
+        Args:
+            size (int): size of the square
+
+        """
+        self.__size = size
