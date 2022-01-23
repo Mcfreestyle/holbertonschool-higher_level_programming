b678b76270f7dcd501760919a0cdcc230d025269
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-22 22:14:47 -0500

    feat: function that prints a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module supplies one function that prints a square
+"""
+
+
+def print_square(size):
+    """ Prints a square with the character #
+
+    Args:
+        size: size of the square
+
+    Raises:
+        TypeError: If size isn't a integer
+        ValueError: If size is less than 0
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
