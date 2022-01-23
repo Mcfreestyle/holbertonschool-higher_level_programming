1aaf46e1910d996c9db069a709315ac81efbbca6
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-22 19:07:40 -0500

    style: module docstring separated of the function definition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,13 +2,15 @@
 """
 This module supplies one function that adds 2 numbers.
 """
+
+
 def add_integer(a, b=98):
     """Adds 2 numbers
 
     Args:
         a: first number
         b: second number
-    
+
     Raises:
         TypeError: If a or b are not integers or floats
 
