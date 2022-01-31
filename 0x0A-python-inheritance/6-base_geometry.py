52bee353343c8f1ab78913f539e968757cda63b9
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-31 16:23:30 -0500

    feat: BaseGeometry class that defines the  method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module supplies the `BaseGeometry` class
+"""
+
+
+class BaseGeometry:
+    """Defines the `area` method"""
+    def area(self):
+        """Raises a Exception"""
+        raise Exception("area() is not implemented")
