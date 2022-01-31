4c8930cca9200257351d2925e13346db182a9238
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-31 18:10:11 -0500

    refactor: use of type function instead of isinstance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
             TypeError: if value isn't an integer
             ValueError: if value is less or equal to 0
         """
-        if not isinstance(value, int):
-            raise TypeError(name + " must be an integer")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
