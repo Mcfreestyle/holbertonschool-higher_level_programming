ddcae85c5c8c80d485917d01f32a35b4de81b8da
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-31 12:30:36 -0500

    feat: returns the attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module supplies a fuction that returns the attributes
+and methods of an object
+"""
+
+
+def lookup(obj):
+    """Returns the attributes and methods of an object"""
+
+    return (dir(obj))
