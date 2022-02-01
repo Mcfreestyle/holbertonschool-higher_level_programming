f43a58eb5de02308a3ef33bdf9eb6aad13480e77
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-02-01 18:44:56 -0500

    style: docstring in two lines

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module adds all arguments to a Python list,
+and then save them to a file
+"""
 
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -7,7 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 try:
     my_list = load_from_json_file(filename)
-except:
+except Exception:
     my_list = []
 
 for arg in argv[1:]:
