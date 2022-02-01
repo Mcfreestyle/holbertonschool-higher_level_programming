01e105bc2df802a217518d99cc59187ce24f7d09
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-02-01 15:08:37 -0500

    feat: returns an object represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module supplies the``from_json_string`` function
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string"""
+    return (json.loads(my_str))
