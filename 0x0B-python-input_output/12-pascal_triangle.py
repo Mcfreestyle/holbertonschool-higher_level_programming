985262f81f4c5fc6b8f29749ec9b51b4a88cd23d
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-02-02 18:44:35 -0500

    feat: creates a list of lists of integers representing the Pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module supplies the ``pascal_triangle`` function
+"""
+
+
+def pascal_triangle(n):
+    """Create a lists of lists of integers representing the Pascal's triangle
+    Args:
+        n: size of the triangle
+    """
+    l1 = [[1]]
+    l2 = []
+    for i in range(1, n):
+        l2 = [1] + [l2[i] + l2[i + 1] for i in range(len(l2) - 1)] + [1]
+        l1.append(l2)
+    return (l1)
