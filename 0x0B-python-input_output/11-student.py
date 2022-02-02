708b3ff30ed0da5656f7159623d6ce3fcc66038f
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-02-02 01:25:17 -0500

    feat: Student class defines the reload_from_json method

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module supplies the ``Student`` class
+"""
+
+
+class Student:
+    """Definition of the class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the instance
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of a Student instance"""
+        if isinstance(attrs, list):
+            my_dict = {}
+            for item in attrs:
+                if item in self.__dict__:
+                    my_dict[item] = self.__dict__[item]
+        else:
+            return (self.__dict__)
+
+        return (my_dict)
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for item in json:
+            self.__dict__[item] = json[item]
