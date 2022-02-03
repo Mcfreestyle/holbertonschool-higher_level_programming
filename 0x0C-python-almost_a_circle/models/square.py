9a41acd53b75c1e2d25e289eab97b4e3a84f1af1
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-02-03 18:00:51 -0500

    feat: definition of the square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module supplies the ``Square`` class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Definition of the class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the object
+
+        Args:
+            size: size of the square
+            x: position x
+            y: position y
+            id: square id
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns the description of the square"""
+        return ("[Square] ({}) {}/{} - {}".
+                format(self.id, self.x, self.y, self.width))
