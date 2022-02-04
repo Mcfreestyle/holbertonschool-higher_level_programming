5aef5571c01e65635678f7a66f67e15b99ae8e10
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-02-04 09:41:25 -0500

    chore: Base class defines the to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module supplies the ``Base`` class
 """
+import json
 
 
 class Base:
@@ -19,3 +20,9 @@ class Base:
         else:
             Base. __nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
