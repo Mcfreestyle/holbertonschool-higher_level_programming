eef57651457525e7e36f39b808ae1024b0f97d04
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-03-10 12:39:23 -0500

    chore: use of ORDER BY clause

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,5 @@ SELECT genres.name
 FROM tv_show_genres shows_genres
 INNER JOIN tv_genres genres ON shows_genres.genre_id = genres.id
 INNER JOIN tv_shows shows ON shows_genres.show_id = shows.id
-WHERE shows.title = "Dexter";
+WHERE shows.title = "Dexter"
+ORDER BY genres.name;
