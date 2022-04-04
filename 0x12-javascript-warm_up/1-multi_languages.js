fe912766f3bdcd2e8019a2b395f239c11f485eb8
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-04 16:51:05 -0500

    style: other value of the variable

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const msgC = 'C is fun';
 const msgPy = 'Python is cool';
-const msgJs = 'Javascript is amazing';
+const msgJs = 'JavaScript is amazing';
 console.log(`${msgC}\n${msgPy}\n${msgJs}`);
