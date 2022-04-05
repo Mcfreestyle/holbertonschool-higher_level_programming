56f8ba4913929586c1b007150c394982faaacd02
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-04 23:24:21 -0500

    fix: use of optional parameter of 'sort' method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,6 +6,6 @@ const size = myArgv.length;
 
 if (size === 0 || size === 1) console.log(0);
 else {
-  myArgv.sort();
-  console.log(myArgv[size - 2]);
+  myArgv.sort(function (a, b) { return (b - a); });
+  console.log(myArgv[1]);
 }
