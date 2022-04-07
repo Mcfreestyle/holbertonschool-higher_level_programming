406efb78e68090148ef97bc7f8e10abcaadeae14
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-07 18:28:04 -0500

    feat: takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. Write one that is safe from MySQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module connect to a MySQL server, takes in an argument
+    and displays all values in the states table of hbtn_0e_0_usa
+    where name matches the argument.
+    Write one that is safe from MySQL injections!
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    _, user, passwd, db, state = sys.argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user,
+                         passwd=passwd,
+                         db=db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state,))
+    logs = cur.fetchall()
+    for log in logs:
+        print(log)
+
+    cur.close()
+    db.close()
