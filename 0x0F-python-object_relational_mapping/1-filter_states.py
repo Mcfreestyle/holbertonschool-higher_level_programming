96d7e5427501b73f8d881292574f1efc73be6c99
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-07 16:01:14 -0500

    feat: lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module connect to a MySQL server and lists all states
+    with a name starting with N from the database `hbtn_0e_0_usa`
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    _, user, passwd, db = sys.argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user,
+                         passwd=passwd,
+                         db=db)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
