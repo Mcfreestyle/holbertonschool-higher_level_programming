10d2179d8d77de75f18f83f7d386f86383f90afd
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-05-17 19:47:01 -0500

    feat: prints the number of movies where the character Wedge Antilles is present, his id is 18

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* prints the number of movies where the character "Wedge Antilles" is present, his id is 18 */
+
+const axios = require('axios');
+const api = process.argv[2];
+const charac = 'https://swapi-api.hbtn.io/api/people/18/';
+
+axios.get(api, { responseType: 'json' })
+  .then(res => {
+    const films = res.data.results;
+    let count = 0;
+
+    for (const film of films) {
+      for (const c of film.characters) {
+        if (charac === c) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  });
