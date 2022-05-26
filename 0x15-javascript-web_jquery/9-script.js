89dcfde86335bf45ebd8240db99e371b3a15fd43
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-05-26 13:16:09 -0500

    refactor: use of ready method in the document object

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,11 @@
 // Fetches from a URL and displays the value of hello from that fetch in a HTML tag
-$(function () {
+function greeting () {
   const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
   const divHello = $('DIV#hello');
 
   $.get(url, ({ hello }) => {
     divHello.text(hello);
   });
-});
+}
+$(document).ready(greeting);
+// $(window).on('load', greeting);
